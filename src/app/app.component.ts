46528a462e9f5fd9462269f38e137ad05cf14779
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2017-07-12 01:42:44 +0200

    toy scorecard "miner" inspired by https://github.com/anders94/blockchain-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+export class Block {
+  previous_hash: string;
+  points: number;
+  nonce: number;
+  hashed: string;
+  hash() {
+    return CryptoJS
+      .SHA256("foobar"+this.previous_hash+","+this.points+","+this.nonce)
+      .toString()
+      .toUpperCase()
+      .substr(0, 4);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Scorecard Miner';
+  block = new Block();
+  chain = [new Block(), new Block(), new Block(), new Block(), new Block(), new Block(), new Block()];
+  mine(block: Block): void {
+    for (var x = 0; x <= 500000; x++) {
+      block.nonce = x;
+      block.hashed = block.hash(); 
+      if (block.hashed.substr(0, 2) === '00') {
+        break;
+      }
+    }
+  }
+  reset(block: Block): void {
+    block.previous_hash = undefined;
+    block.points = undefined;
+    block.nonce = undefined;
+    block.hashed = undefined;
+  }
+  validate(): void {
+    // todo
+  }
 }
